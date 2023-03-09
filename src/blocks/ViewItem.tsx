e3a07299611ae2b3513f906a0b7dc2022805b63b
Emily Jablonski <emily.jablonski@exygy.com>
2023-03-09 13:42:11 -0700

    fix: align ViewItem with detroit updates

--- src/blocks/ViewItem.tsx
+++ src/blocks/ViewItem.tsx
@@ -11,6 +11,7 @@ export interface ViewItemProps {
   truncated?: boolean
   error?: boolean
   dataTestId?: string
+  labelClassName?: string
 }
 
 const ViewItem = (props: ViewItemProps) => {
@@ -25,7 +26,11 @@ const ViewItem = (props: ViewItemProps) => {
   return (
     <div id={props.id} className={viewItemClasses.join(" ")} data-test-id={props.dataTestId}>
       {props.label && (
-        <span className={`view-item__label ${props.error ? "text-alert text-sm" : ""}`}>
+        <span
+          className={`view-item__label ${props.labelClassName || ""} ${
+            props.error ? "view-item__label-error" : ""
+          }`}
+        >
           {props.label}
         </span>
       )}
