b62b538f187298a9eb5fe2ad66df77b70bb9bb9c
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2023-03-17 16:41:01 -0600

    fix: align LanguageNav with Detroit updates (#68)

--- src/navigation/LanguageNav.stories.tsx
+++ src/navigation/LanguageNav.stories.tsx
@@ -1,11 +1,14 @@
 import React from "react"
-
+import { BADGES } from "../../.storybook/constants"
 import { LanguageNav } from "./LanguageNav"
 import { text, withKnobs } from "@storybook/addon-knobs"
 
 export default {
-  title: "Navigation/LanguageNav",
+  title: "Navigation/LanguageNav 🚩",
   decorators: [(storyFn: any) => <div style={{ padding: "1rem" }}>{storyFn()}</div>, withKnobs],
+  parameters: {
+    badges: [BADGES.GEN2],
+  },
 }
 
 export const Default = () => (
