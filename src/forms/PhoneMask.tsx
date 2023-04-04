b4d584a7964e57bea5a20ac153e7ccc420cf7f60
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2023-04-04 12:36:44 -0600

    fix: fixup react types (#94)

--- src/forms/PhoneMask.tsx
+++ src/forms/PhoneMask.tsx
@@ -5,22 +5,24 @@ export const PhoneMask = React.forwardRef((props: any, ref: any) => {
   const { value, onChange, name, disabled, placeholder } = props
 
   return (
-    <MaskedInput
-      mask={["(", /[1-9]/, /\d/, /\d/, ")", " ", /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]}
-      className="input"
-      type="tel"
-      placeholder={placeholder || "(555) 555-5555"}
-      guide={false}
-      id={name}
-      value={value}
-      name={name}
-      disabled={disabled}
-      onChange={(e: any) => {
-        e.persist()
-        onChange(e)
-      }}
-      ref={ref}
-      aria-labelledby={"phone-label"}
-    />
+    <>
+      <MaskedInput
+        mask={["(", /[1-9]/, /\d/, /\d/, ")", " ", /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]}
+        className="input"
+        type="tel"
+        placeholder={placeholder || "(555) 555-5555"}
+        guide={false}
+        id={name}
+        value={value}
+        name={name}
+        disabled={disabled}
+        onChange={(e: any) => {
+          e.persist()
+          onChange(e)
+        }}
+        ref={ref}
+        aria-labelledby={"phone-label"}
+      />
+    </>
   )
 })
