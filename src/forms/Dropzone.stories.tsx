6e36b44fe24dc74290883a8e93ef6a105e7146bb
Emily Jablonski <emily.jablonski@exygy.com>
2023-04-12 11:38:32 -0600

    fix: remove cloudinary references

--- src/forms/Dropzone.stories.tsx
+++ src/forms/Dropzone.stories.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
-import { withKnobs, text } from "@storybook/addon-knobs"
-import { CloudinaryUpload } from "./CloudinaryUpload"
+import { withKnobs } from "@storybook/addon-knobs"
 import { Dropzone } from "./Dropzone"
 
 export default {
@@ -9,44 +8,16 @@ export default {
 }
 
 export const defaultDropzone = () => {
-  const [progressValue, setProgressValue] = React.useState(0)
-  const [cloudinaryImage, setCloudinaryImage] = React.useState("")
-  const cloudName = text("Cloudinary Cloud", "")
-  const uploadPreset = text("Upload Preset", "")
-
-  const exampleUploader = (file: File) => {
-    CloudinaryUpload({
-      file: file,
-      onUploadProgress: (progress) => {
-        setProgressValue(progress)
-      },
-      cloudName,
-      uploadPreset,
-    }).then((response) => {
-      setProgressValue(100)
-      const imgUrl = `https://res.cloudinary.com/${cloudName}/image/upload/w_400,c_limit,q_65/${response.data.public_id}.jpg`
-      setCloudinaryImage(imgUrl)
-    })
-  }
-
+  const progressValue = 50
   return (
     <>
       <Dropzone
         id="test-uploading"
         label="Upload File"
         helptext="Select JPEG or PNG files"
-        uploader={exampleUploader}
+        uploader={() => {}}
         accept="image/*"
         progress={progressValue}
-      />
-      {progressValue == 0 && (
-        <p className="mt-16">(Provide Cloudinary credentials via the Knobs below.)</p>
-      )}
-      <img
-        src={cloudinaryImage}
-        style={{ width: "200px" }}
-        role={cloudinaryImage ? undefined : "none"}
-        alt={cloudinaryImage && "uploaded file"}
       />
     </>
   )
