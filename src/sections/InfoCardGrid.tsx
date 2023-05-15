74545ac33eefce7b92936650ac6574154f4cacd7
Cade Wolcott <cade.wolcott@exygy.com>
2023-05-15 12:23:20 -0500

    feat: add headerStyle component to InfoCardGrid

--- src/sections/InfoCardGrid.tsx
+++ src/sections/InfoCardGrid.tsx
@@ -1,17 +1,22 @@
 import * as React from "react"
-import { Heading } from "../text/Heading"
+import { HeaderType, Heading } from "../text/Heading"
 import "./InfoCardGrid.scss"
 
 export interface InfoCardGridProps {
   title: string
   subtitle?: string
+  headingStyle?: HeaderType
   children: React.ReactNode
 }
 
 const InfoCardGrid = (props: InfoCardGridProps) => (
   <section className="info-cards">
     <header className="info-cards__header">
-      <Heading styleType={"underlineWeighted"} priority={2} className={"info-cards__title"}>
+      <Heading
+        styleType={props.headingStyle || "underlineWeighted"}
+        priority={2}
+        className={"info-cards__title"}
+      >
         {props.title}
       </Heading>
       {props.subtitle && <p className="info-cards__subtitle">{props.subtitle}</p>}
