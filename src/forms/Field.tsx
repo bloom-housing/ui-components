8197b0e1ecd362e93b4ff3c130ee0ece270410f1
krzysztof ziecina <kziecina@airnauts.com>
2023-05-25 15:36:03 +0200

    Revert "fix: add url type to field with url validation"
    This reverts commit 5c46043a2abe1529f69355032130957b60fdf141.

--- src/forms/Field.tsx
+++ src/forms/Field.tsx
@@ -1,7 +1,6 @@
 import React, { ChangeEvent, useMemo } from "react"
 import { ErrorMessage } from "../notifications/ErrorMessage"
 import { UseFormMethods, RegisterOptions } from "react-hook-form"
-import { isURL } from "class-validator"
 
 export interface FieldProps {
   error?: boolean
@@ -90,19 +89,6 @@ const Field = (props: FieldProps) => {
       onBlur: () => formatValue(),
       onFocus: () => formatValue(true),
       onChange: filterNumbers,
-    }
-  }
-
-  if (props.type === "url") {
-    inputProps = {
-      ...inputProps,
-      ref:
-        props.register &&
-        props.register(
-          props.validation || {
-            validate: (value) => isURL(value, { require_protocol: true }),
-          }
-        ),
     }
   }
 
