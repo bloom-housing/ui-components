4f48af96b44b1e96fc02c458da8678ed107c3eb9
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2025-07-29 13:01:06 -0600

    fix: allow JSX in TimeField label (#174)

--- src/forms/TimeField.stories.tsx
+++ src/forms/TimeField.stories.tsx
@@ -23,3 +23,24 @@ export const Default = () => {
     />
   )
 }
+
+export const FormattedLabel = () => {
+  const { register, setValue, watch, errors } = useForm({ mode: "onChange" })
+
+  return (
+    <TimeField
+      id="time"
+      label={
+        <>
+          Time <strong className={"text-alert"}>*</strong>
+        </>
+      }
+      name="time"
+      required={true}
+      register={register}
+      setValue={setValue}
+      watch={watch}
+      error={!!errors?.time}
+    />
+  )
+}
